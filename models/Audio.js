b562f778dcd95ee46d08c76b7db45fff1cf89c66
nickhsine <nickhsine97753017@gmail.com>
2016-06-05 12:06:11 +0800

    Update models/Audio.js

--- models/Audio.js
+++ models/Audio.js
@@ -3,27 +3,29 @@ var transform = require('model-transform');
 var Types = keystone.Field.Types;
 
 var Audio = new keystone.List('Audio', {
-  map: { name: 'description' }
+    map: { name: 'title' }
 });
 
 Audio.add({
-  description: { type: String, required: true, initial: true },
-  audio: {
-    type: Types.GcsFile,
-    initial: true,
-    index: true,
-    autoCleanup: true,
-    datePrefix: 'YYYYMMDDHHmmss',
-    // TODO move these settings to config
-    bucket: 'twreporter-multimedia',
-    destination: 'audios/',
-    publicRead: true,
-  },
-  tags: {
-    type: Types.Relationship,
-    ref: 'Tag',
-    many: true
-  },
+    title: { type: String, required: true, initial: true },
+    description: { type: String, required: true, initial: true },
+    audio: {
+        type: Types.GcsFile,
+        initial: true,
+        index: true,
+        autoCleanup: true,
+        datePrefix: 'YYYYMMDDHHmmss',
+        // TODO move these settings to config
+        bucket: 'twreporter-multimedia',
+        destination: 'audios/',
+        publicRead: true,
+    },
+    coverPhoto: { type: Types.ImageRelationship, ref: 'Image' },
+    tags: {
+        type: Types.Relationship,
+        ref: 'Tag',
+        many: true
+    },
 });
 
 
