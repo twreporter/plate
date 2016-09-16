ccc773de40477752d4b3ebe48cd8b2ebc722d5fb
nickhsine <nickhsine97753017@gmail.com>
2016-09-16 15:54:18 +0800

    LeadingVideo and Bookmarks for longform article

---
+++ models/Video.js
@@ -0,0 +1,33 @@
+var config = require('../config');
+var keystone = require('twreporter-keystone');
+var transform = require('model-transform');
+var Types = keystone.Field.Types;
+
+var Video = new keystone.List('Video', {
+    map: { name: 'title' }
+});
+var bucket = config['options']['gcs config']['bucket']
+
+Video.add({
+    title: { type: String, required: true, initial: true },
+    description: { type: Types.Html, wysiwyg: true, height: 150 },
+    video: {
+        type: Types.GcsFile,
+        initial: true,
+        autoCleanup: true,
+        datePrefix: 'YYYYMMDDHHmmss',
+        bucket: bucket,
+        destination: 'videos/',
+        publicRead: true,
+    },
+    tags: {
+        type: Types.Relationship,
+        ref: 'Tag',
+        many: true
+    },
+});
+
+
+transform.toJSON(Video);
+Video.defaultColumns = 'title, video, tags';
+Video.register();
