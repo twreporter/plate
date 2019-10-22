a47bc440329125205e0082e70f7b67af1757c2e0
nickhsine <nickhsine97753017@gmail.com>
2019-10-22 16:18:56 +0800

    integration with CircleCI

---
+++ config-for-docker-build.js
@@ -0,0 +1,35 @@
+exports.options = {
+  'name': 'Keystone',
+  'brand': 'Keystone',
+  'less': 'public',
+  'static': 'public',
+  'favicon': 'public/favicon.ico',
+  'views': 'templates/views',
+  'view engine': 'jade',
+  'mongo': process.env.KEYSTONE_MONGODB,
+  'cloudinary config': 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo',
+  'auto update': true,
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': process.env.KEYSTONE_COOKIE_SECRET,
+  'gcs config': {
+    projectId: process.env.KEYSTONE_GCS_PROJECT_ID,
+    keyFilename: __dirname + '/gcskeyfile.json',
+    bucket: process.env.KEYSTONE_GCS_BUCKET,
+  },
+  'preview config': {
+    protocol: 'http',
+    host: 'twreporter-react', // Using host entries created by Docker in /etc/hosts
+    port: 3000
+  },
+};
+
+exports.locals = {
+  env: process.env,
+};
+
+exports.nav = {
+  'people': ['users', 'companies', 'contacts'],
+  'content': ['posts', 'post-categories', 'topics', 'image-collections', 'images', 'audios', 'videos', 'tags'],
+};
