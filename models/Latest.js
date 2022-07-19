674c7bc257de7795d5f646447eacf6e4acb21ecc
duidae <duidae@gmail.com>
2022-07-19 15:57:24 +0800

    feat: enable latest page

---
+++ models/Latest.js
@@ -0,0 +1,15 @@
+var keystone = require('@twreporter/keystone');
+var transform = require('model-transform');
+
+var Latest = new keystone.List('Latest', {
+	autokey: { from: 'name', path: 'key', unique: true },
+});
+
+Latest.add({
+	name: { label: '最新', type: String, required: true },
+});
+
+Latest.relationship({ ref: 'Post', refPath: 'latests' });
+
+transform.toJSON(Latest);
+Latest.register();
