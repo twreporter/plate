44e0d5d657536a9bb0eb0c5c35f03cee720e1acb
duidae <duidae@gmail.com>
2022-08-16 17:03:40 +0800

    fix: disable subcategory editing

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -9,7 +9,7 @@ var PostCategory = new keystone.List('PostCategory', {
 
 PostCategory.add({
 	name: { label: "名稱", type: String, required: true },
-	subcategory: { label: '子分類', type: Types.Relationship, ref: 'Tag', many: true }
+	subcategory: { label: '子分類', type: Types.Relationship, ref: 'Tag', many: true, noedit: true }
 });
 
 PostCategory.relationship({ ref: 'Post', refPath: 'categories' });
