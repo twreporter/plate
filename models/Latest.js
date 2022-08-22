b627ee2b17525f4ccfd74c85c5b2b60beb1c94b1
duidae <duidae@gmail.com>
2022-08-22 17:52:25 +0800

    feat: make latest sortable & comment out relationship config

--- models/Latest.js
+++ models/Latest.js
@@ -3,13 +3,14 @@ var transform = require('model-transform');
 
 var Latest = new keystone.List('Latest', {
 	autokey: { from: 'name', path: 'key', unique: true },
+	sortable: true,
 });
 
 Latest.add({
 	name: { label: '最新', type: String, required: true },
 });
 
-Latest.relationship({ ref: 'Post', refPath: 'latests' });
+// Latest.relationship({ ref: 'Post', refPath: 'latests' });
 
 transform.toJSON(Latest);
 Latest.register();
