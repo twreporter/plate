5ed10b87d3744f9cf8fc3c22e70f99c3cb90d4a4
duidae <duidae@gmail.com>
2022-08-29 17:34:03 +0800

    fix: connect latest with tags

--- models/Latest.js
+++ models/Latest.js
@@ -1,9 +1,18 @@
 var keystone = require('@twreporter/keystone');
 var transform = require('model-transform');
 
+const tagsCollectionName = 'tags';
+
 var Latest = new keystone.List('Latest', {
+	schema: {
+		collection: keystone.prefixModel(tagsCollectionName),
+		// Disable the autoIndex option in production
+		// https://mongoosejs.com/docs/guide.html#autoIndex
+		autoIndex: keystone.get('env') !== 'production',
+	},
 	autokey: { from: 'name', path: 'key', unique: true },
 	sortable: true,
+    nodelete: true,
 });
 
 Latest.add({
