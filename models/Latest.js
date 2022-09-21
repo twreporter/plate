a428bed880b4e379428f936d2f49acb8e357e893
duidae <duidae@gmail.com>
2022-09-21 11:36:30 +0800

    fix: fix lastest order field

--- models/Latest.js
+++ models/Latest.js
@@ -13,10 +13,12 @@ var Latest = new keystone.List('Latest', {
 	autokey: { from: 'name', path: 'key', unique: true },
 	sortable: true,
     nodelete: true,
+	preFilter: { latest_order: { mode: 'gt', value: 0 }}, // TODO
 });
 
 Latest.add({
 	name: { label: '最新', type: String, required: true },
+	latest_order: { label: '最新(順序)', type: Number, noedit: true },
 });
 
 transform.toJSON(Latest);
