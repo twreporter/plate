74957c3cf868036236391975c9e8af4a8464fb01
duidae <duidae@gmail.com>
2023-01-10 23:52:30 +0800

    fix: fix latest config

--- models/Latest.js
+++ models/Latest.js
@@ -1,36 +1,14 @@
 var keystone = require('@twreporter/keystone');
 var transform = require('model-transform');
-var Types = keystone.Field.Types;
-
-const tagsCollectionName = 'tags';
 
 var Latest = new keystone.List('Latest', {
-	schema: {
-		collection: keystone.prefixModel(tagsCollectionName),
-		// Disable the autoIndex option in production
-		// https://mongoosejs.com/docs/guide.html#autoIndex
-		autoIndex: keystone.get('env') !== 'production',
-	},
 	autokey: { from: 'name', path: 'key', unique: true },
-	sortable: true,
-	preFilter: { // TODO: check fields
-		field: {
-			path: 'latest_order',
-			label: "最新(順序)",
-			size: "small",
-			type: "number"
-		},
-		value: { mode: 'gt', value: 0 }
-	},
 });
 
 Latest.add({
 	name: { label: '最新', type: String, required: true },
-	postNum: { label: '文章數量', type: Number, noedit: true },
-	latestDate: { label: '最新文章日期', type: Types.Date, noedit: true },
-	latest_order: { label: '最新(順序)', type: Number, noedit: true },
+	history: { label: '編輯紀錄', type: String, noedit: true },
 });
-Latest.defaultColumns = 'name, postNum, latestDate';
 
 transform.toJSON(Latest);
 Latest.register();
